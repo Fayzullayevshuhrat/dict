4efd44d493033be050fd52ddca350194a022dca8
shuhrat <Fayzullayevshuhrat0904@gmail.com>
2025-01-20 20:47:24 +0500

    dict

--- problem2.py
+++ problem2.py
@@ -1,63 +1,85 @@
-class Company:
-    def init(self, cn, year, founder):
-        self.cn = cn
-        self.year = year
-        self.founder = founder
-
-    def to_dict(self):
-        return {
-            "company_name": self.cn,
-            "year": self.year,
-            "founder": self.founder,
-        }
 
 
-class Product:
-    def init(self, pn, price, amount, company):
-        self.pn = pn
-        self.price = price
-        self.amount = amount
-        self.company = company
+class Pupil:
+    def __init__(self, name, yil, sinf):
+        self.name = name
+        self.yil = yil
+        self.sinf = sinf
 
-    def str(self):
-        return f"Mahsulot: {self.pn}, Narxi: {self.price}, Miqdori: {self.amount}, Kompaniya: {self.company.cn}"
-
-    def to_dict(self):
-        return {
-            "product_name": self.pn,
-            "price": self.price,
-            "amount": self.amount,
-            "company": self.company.to_dict(),
-        }
+    def info(self):
+        return f"Pupil Name: {self.name}, Year: {self.yil}, Class: {self.sinf}"
 
 
-class Basket:
-    def init(self):
-        self.products = []
+class Student:
+    def __init__(self, name, yil, kurs, fakultet):
+        self.name = name
+        self.yil = yil
+        self.kurs = kurs
+        self.fakultet = fakultet
 
-    def add(self, product):
-        for i in self.products:
-            if i.pn == product.pn:
-                i.amount += product.amount
-                return
+    def info(self):
+        return f"Student Name: {self.name}, Year: {self.yil}, Course: {self.kurs}, Faculty: {self.fakultet}"
 
-        self.products.append(product)
 
-    def remove(self, product_name):
-        self.products = [p for p in self.products if p.pn != product_name]
+class PDPManagement:
+    def __init__(self):
+        self.students = {}
+        self.pupils = {}
 
-    def view(self):
-        for i in self.products:
-            print(i)
+    def add_st(self, student_id, student):
+        self.students[student_id] = student
 
-    def total(self):
-        summa = 0
-        for product in self.products:
-            summa += product.price * product.amount
-        return summa
+    def add_pu(self, pupil_id, pupil):
+        self.pupils[pupil_id] = pupil
 
-    def to_dict(self):
-        return {
-            "products": [product.to_dict() for product in self.products],
-            "total_price": self.total(),
-        }
+    def remove_st(self, student_id):
+        if student_id in self.students:
+            del self.students[student_id]
+
+    def remove_pu(self, pupil_id):
+        if pupil_id in self.pupils:
+            del self.pupils[pupil_id]
+
+    def info_st(self):
+        return [student.info() for student in self.students.values()]
+
+    def info_pu(self):
+        return [pupil.info() for pupil in self.pupils.values()]
+
+
+# Example usage:
+pdp_management = PDPManagement()
+
+# Adding pupils
+pupil1 = Pupil(name="Bobur", yil=2025, sinf="5th")
+pupil2 = Pupil(name="Aziz", yil=2025, sinf="6th")
+pdp_management.add_pu(pupil_id=1, pupil=pupil1)
+pdp_management.add_pu(pupil_id=2, pupil=pupil2)
+
+# Adding students
+student1 = Student(name="Aslbek", yil=2025, kurs="1st", fakultet="Computer Science")
+student2 = Student(name="sherbek", yil=2025, kurs="2nd", fakultet="Mathematics")
+pdp_management.add_st(student_id=1, student=student1)
+pdp_management.add_st(student_id=2, student=student2)
+
+# Printing information
+print("Pupils Info:")
+for info in pdp_management.info_pu():
+    print(info)
+
+print("\nStudents Info:")
+for info in pdp_management.info_st():
+    print(info)
+
+# Removing a pupil and a student
+pdp_management.remove_pu(1)
+pdp_management.remove_st(2)
+
+# Printing updated information
+print("\nUpdated Pupils Info:")
+for info in pdp_management.info_pu():
+    print(info)
+
+print("\nUpdated Students Info:")
+for info in pdp_management.info_st():
+    print(info)
